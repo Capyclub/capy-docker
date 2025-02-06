242864586ae4f3e3356aae37e4617fb25d801d66
tommosti <tommosti1@gmail.com>
2025-02-06 15:20:35 +0100

    fix : delete fake front repo

--- capy-front/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import {render} from "@testing-library/react";
-import App from "./App";
-
-describe('App integration tests', () => {
-    it('RegisterForm', () => {
-        render(<App/>);
-
-    });
-});
